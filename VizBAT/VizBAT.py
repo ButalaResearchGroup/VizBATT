f2f9408b2f5a8143a7053044127ab0bb22718387
dnalverson <88463574+dnalverson@users.noreply.github.com>
2024-12-05 15:33:54 -0500

    ability to run locally

--- VizBAT/VizBAT.py
+++ VizBAT/VizBAT.py
@@ -41,7 +41,7 @@ import simpleml as sml
 
 #os.chdir(r'C:\Users\dalverson\Dropbox (UFL)\Butala Hackathon Team\Danielles Code Folder\wrpoly\data')
 
-merged_df = pd.read_csv('battery_structure_performance', index_col=0)
+merged_df = pd.read_csv('battery_structure_performance.csv', index_col=0)
 #merged_df['Octahedral Distortion?'] = merged_df['Octahedral Distortion?'].map({'No': 0, 'Yes': 1})
 unique_df = merged_df.drop_duplicates(subset='cif_name', keep='first').copy()
 #unique_df['Octahedral Distortion?'] = unique_df['Octahedral Distortion?'].map({'No': 0, 'Yes': 1})
@@ -148,7 +148,6 @@ def b64_image(image_filename):
     
 
 # Assume 'unique_df' is your dataframe with data
-
 # Create Dash app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 
